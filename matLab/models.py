ab30682e48bfeaa734399a2332a5dc1ea5aa4e01
leonie-boullier <leonie.boullier@u-psud.fr>
2018-04-03 10:17:27 +0200

    MatLap Licence app, first commit

---
+++ matLab/models.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Licence(models.Model):
+	BATIMENTS = (
+		('GeePs', 'GeePs'),
+		('Breguet', 'Breguet'),
+	)
+	MATLABRELEASES = (
+		('R2017b', 'R2017b'),
+		('R2016b', 'R2016b'),
+		('R2015b', 'R2015b'),
+		('R2014b', 'R2014b'),
+		('R2013b', 'R2013b'),
+	)
+	
+	SYSTEMS = (
+		('Windows', 'Windows'),
+		('MacOS', 'MacOS'),
+		('Linux', 'Linux'),
+	)
+	
+	name = models.CharField(max_length=50)
+	firstname = models.CharField(max_length=50)
+	email = models.EmailField(max_length=50)
+	telephone = models.CharField(max_length=10)
+	bureau = models.CharField(max_length=200)
+	batiment = models.CharField(max_length=10, choices=BATIMENTS)
+	matlabRelease = models.CharField(max_length=10, choices=MATLABRELEASES)
+	operatingSystem = models.CharField(max_length=10 , choices=SYSTEMS)
+	hostID = models.IntegerField()
+	
+	def __str__(self):
+		return self.firstname+' '+self.name
