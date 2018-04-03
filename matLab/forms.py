241bead3de1e45c23fd31addb3e62ea926a8ef43
leonie-boullier <leonie.boullier@u-psud.fr>
2018-04-03 10:55:13 +0200

    Mise en place du formulaire

---
+++ matLab/forms.py
@@ -0,0 +1,8 @@
+
+from django import forms
+from .models import Licence
+
+class LicenceForm(forms.ModelForm):
+	class Meta:
+		model = Licence
+		fields = ('firstname','name', 'email', 'telephone', 'bureau', 'batiment', 'matlabRelease', 'operatingSystem', 'hostID',)
--- matLab/views.py
+++ matLab/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .forms import LicenceForm
 
 # Create your views here.
+
+def formulaire(request):
+	form = LicenceForm()
+	return render(request, 'matLab/formulaire.html', {'form':form})
