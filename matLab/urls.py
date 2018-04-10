8a3718ef08072b3ae1c453fbab97fe5b85b3aaf3
Zendolin <josh.valmy@gmail.com>
2018-04-10 16:31:17 +0200

    Ajout details des renouvellement de licence

--- matLab/urls.py
+++ matLab/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.formulaire, name='formulaire'),
+    url(r'^licence/(?P<pk>[0-9]+)/$', views.licence_detail, name='licence_detail'),
 ]
--- matLab/views.py
+++ matLab/views.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import LicenceForm
+from .models import Licence
 
 # Create your views here.
 
 def formulaire(request):
-	form = LicenceForm()
+	if request.method == "POST":
+		form = LicenceForm(request.POST)
+		if form.is_valid():
+			licence = form.save(commit=False)
+			licence.save()
+			return redirect('licence_detail', pk=licence.pk)
+	else:
+		form = LicenceForm()
 	return render(request, 'matLab/formulaire.html', {'form':form})
+
+def licence_detail(request, pk):
+	licence = get_object_or_404(Licence, pk=pk)
+	return render(request, 'matLab/licence_details.html', {'licence':licence})
